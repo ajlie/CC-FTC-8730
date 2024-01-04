d7557c07d57251ddd196593848d4f0fd0215a3dd
Burwasser <lburwasser@philasd.org>
2024-01-04 15:25:25 -0500

    Added OpenCV Pipeline

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/TeamElementDetection/Pipeline/SplitAveragePipeline.java
@@ -0,0 +1,2 @@
+package org.firstinspires.ftc.teamcode.Subsystem.TeamElementDetection.Pipeline;public class SplitAveragePipeline {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem/TeamElementDetection/TeamElementSubsystem.java
@@ -0,0 +1,2 @@
+package org.firstinspires.ftc.teamcode.Subsystem.TeamElementDetection;public class TeamElementSubsystem {
+}
